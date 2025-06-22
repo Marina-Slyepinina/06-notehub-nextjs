86c91bffd9f03ddd46757d9dbcd7364f1bee9f0f
Marina <marina.slepinina3@gmail.com>
2025-06-22 18:09:17 +0300

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
 import "./globals.css";
-import TanStackProvider from "@/TanStackProvider/TanStackProvider";
+import TanStackProvider from "@/components/TanStackProvider/TanStackProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
